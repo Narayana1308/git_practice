54b5be77f686d6d6dc0ba60b67b9d2490722f665
Narayana A <narayana@striketru.com>
2026-03-11 20:48:29 +0530

    Practice reset code changes

---
+++ src/main/java/com/learning/git/PracticeGit/Dto/Test.java
@@ -0,0 +1,9 @@
+package com.learning.git.PracticeGit.Dto;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Git Practice");
+        System.out.println("Practice t2o");
+    }
+
+}
