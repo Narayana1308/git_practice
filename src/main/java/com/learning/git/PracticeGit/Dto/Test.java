fbb4158eafb69f5d5fd1fbcef3100db6c638ea27
Narayana A <narayana@striketru.com>
2026-03-11 20:56:30 +0530

    After resolving the confliict push code to git

--- src/main/java/com/learning/git/PracticeGit/Dto/Test.java
+++ src/main/java/com/learning/git/PracticeGit/Dto/Test.java
@@ -3,10 +3,7 @@ package com.learning.git.PracticeGit.Dto;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Git Practice");
-<<<<<<< HEAD
         System.out.println("Practice t2o");
-=======
->>>>>>> b140d9140b3b20f59f8501fd135a11ee99d5b570
     }
 
 }
